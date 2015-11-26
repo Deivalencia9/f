ffc7ff5663ddb954fbf282f9a4db2aac7e81eb9b
David <you@example.com>
2015-11-26 12:23:03 +0100

    Clase MailClient creada

--- MailClient.java
+++ MailClient.java
@@ -1,33 +1,60 @@
 
-/**
- * Write a description of class MailClient here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class MailClient
 {
-    // instance variables - replace the example below with your own
-    private int x;
+	//El servidor al que esta conectado este cliente
+	private MailServer server;
+	//La direccion del usuario que esta usando este cliente
+	private String user;
 
-    /**
-     * Constructor for objects of class MailClient
-     */
-    public MailClient()
-    {
-        // initialise instance variables
-        x = 0;
-    }
+	/**
+	 * Crea un objeto MailClient a partir de los valores dados
+	 */
+	public MailClient(MailServer servidorX, String usuarioX)
+	{
+		user = usuarioX;
+		server = servidorX;
+	}
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+	/**
+	 * Recupera del servidor el siguiente email destinado
+	 * al usuario que esta usando el cliente. Si no hay 
+	 * ningun email pendiente de ser descargado devuelve null; si
+	 * lo hay, devuelve el email
+	 */
+	public MailItem getNextMailItem()
+	{
+		return server.getNextMailItem(user);
+	}
+	
+	
+	/**
+	 * Recupera del servidor el siguiente email destinado al
+	 * usuario que esta usando el cliente e imprime sus datos
+	 * por pantalla. Si no hay ningun email imprime por pantalla
+	 * un mensaje advirtiendo de ello
+	 */
+	public void printNextMailItem()
+	{
+		MailItem email = getNextMailItem();
+		if (email != null)
+		{
+			//Imprimimos los detalles del email
+			email.print();
+		}
+		else 
+		{
+			//Avisamos de que no hay emails en el servidor
+			System.out.println("No hay correo nuevo");	
+		}
+	}
+
+	/**
+	 * Permite redactar un email indicando el destinatario y el
+	 * cuerpo del mensaje y enviarlo al servidor
+	 */
+	public void sendMailItem(String to, String message)
+	{
+		MailItem email = new MailItem(user, to, message);
+		server.post(email);
+	}
 }
