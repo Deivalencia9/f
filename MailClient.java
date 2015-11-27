ef1cbac2f56775a280bda488f18806c590a85e3b
David <you@example.com>
2015-11-27 13:09:38 +0100

    Añadido metodo que muestra el numero de Correos recibidos en un servidor

--- MailClient.java
+++ MailClient.java
@@ -1,60 +1,67 @@
 
 public class MailClient
 {
-	//El servidor al que esta conectado este cliente
-	private MailServer server;
-	//La direccion del usuario que esta usando este cliente
-	private String user;
+    //El servidor al que esta conectado este cliente
+    private MailServer server;
+    //La direccion del usuario que esta usando este cliente
+    private String user;
 
-	/**
-	 * Crea un objeto MailClient a partir de los valores dados
-	 */
-	public MailClient(MailServer servidorX, String usuarioX)
-	{
-		this.user = user;
-		this.server = server;
-	}
+    /**
+     * Crea un objeto MailClient a partir de los valores dados
+     */
+    public MailClient(MailServer servidorX, String usuarioX, int emailsX)
+    {
+        this.user = user;
+        this.server = server;
+    }
 
-	/**
-	 * Recupera del servidor el siguiente email destinado
-	 * al usuario que esta usando el cliente. Si no hay 
-	 * ningun email pendiente de ser descargado devuelve null; si
-	 * lo hay, devuelve el email
-	 */
-	public MailItem getNextMailItem()
-	{
-		return server.getNextMailItem(user);
-	}
-	
-	
-	/**
-	 * Recupera del servidor el siguiente email destinado al
-	 * usuario que esta usando el cliente e imprime sus datos
-	 * por pantalla. Si no hay ningun email imprime por pantalla
-	 * un mensaje advirtiendo de ello
-	 */
-	public void printNextMailItem()
-	{
-		MailItem email = getNextMailItem();
-		if (email != null)
-		{
-			//Imprimimos los detalles del email
-			email.print();
-		}
-		else 
-		{
-			//Avisamos de que no hay emails en el servidor
-			System.out.println("No hay correo nuevo");	
-		}
-	}
+    /**
+     * Recupera del servidor el siguiente email destinado
+     * al usuario que esta usando el cliente. Si no hay 
+     * ningun email pendiente de ser descargado devuelve null; si
+     * lo hay, devuelve el email
+     */
+    public MailItem getNextMailItem()
+    {
+        return server.getNextMailItem(user);
+    }
+    
+    /**
+     * Indica el numero de correos en el servidor
+     */
+    public void numeroCorreos() 
+    {
+        System.out.println("Tienes " + server.howManyMailItems(user) + "Correos ");
+    }
 
-	/**
-	 * Permite redactar un email indicando el destinatario y el
-	 * cuerpo del mensaje y enviarlo al servidor
-	 */
-	public void sendMailItem(String to, String message, String subject)
-	{
-		MailItem email = new MailItem(user, to, message, subject);
-		server.post(email);
-	}
+    /**
+     * Recupera del servidor el siguiente email destinado al
+     * usuario que esta usando el cliente e imprime sus datos
+     * por pantalla. Si no hay ningun email imprime por pantalla
+     * un mensaje advirtiendo de ello
+     */
+    public void printNextMailItem()
+    {
+        MailItem email = getNextMailItem();
+        if (email != null)
+        {
+            //Imprimimos los detalles del email
+            email.print();
+        }
+        else 
+        {
+            //Avisamos de que no hay emails en el servidor
+            System.out.println("No hay correo nuevo");	
+        }
+    }
+
+    /**
+     * Permite redactar un email indicando el destinatario y el
+     * cuerpo del mensaje y enviarlo al servidor
+     */
+    public void sendMailItem(String to, String message, String subject)
+    {
+        MailItem email = new MailItem(user, to, message, subject);
+        server.post(email);
+    }
 }
